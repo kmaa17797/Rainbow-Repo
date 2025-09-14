13aa84258cf2b3ebb09757b02e212e5af425c91a
kmaa17797 <kmaa17797@gmail.com>
2025-09-13 22:05:32 -0400

    Add files via upload
    game files

---
+++ green.gd
@@ -0,0 +1,12 @@
+extends Node
+
+@onready var marker_2d: Marker2D = $Marker2D
+
+
+
+func _on_ready() -> void:
+	for player in get_tree().get_nodes_in_group("player"):
+		player.respawned.connect(_on_player_respawned.bind(player))
+
+func _on_player_respawned(player):
+	player.global_position= marker_2d.global_position
